efa145b55be8222f968537e9591f746bd0d464fa
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2021-12-13 12:06:51 +0300

    Update dvdrentalOdev3.sql

--- dvdrentalOdev3.sql
+++ dvdrentalOdev3.sql
@@ -3,6 +3,6 @@ select * from country where country like 'A%a'
 country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
 select * from country where country like '_____%n'
 film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
-select *  from film where title ~~* '%T%'
+select *  from film where title ilike '%T%T%T%T%'
 film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 select * from film where title like 'c%' and length>90 and rental_rate =2.99
