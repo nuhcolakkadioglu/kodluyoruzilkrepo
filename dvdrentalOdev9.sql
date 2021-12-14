1b240c57676048014abdddd4bfac7f802c8fcca4
Nuh Çolakkadıoğlu <n.colakkadioglu@gmail.com>
2021-12-14 09:54:49 +0300

    create dvdrentalOdev9.sql

---
+++ dvdrentalOdev9.sql
@@ -0,0 +1,5 @@
+city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+select * from city inner join country on city.countryId = country.id
+customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+select first_name,last_name from customer inner join payment on customer.id =payment.payment_id
+customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
